98c9ea7d373f8d470471c5a9c16f0a23a410c51a
Anatoly Anatoly <lptolik@gmail.com>
2021-08-17 13:32:08 +0100

    package documentation

---
+++ R/synaptome_db.R
@@ -0,0 +1,8 @@
+#' synamptome.db: programmatic access to the Synaptic proteome database
+#'
+#' The package contains local copy of the Synaptic proteome database together
+#' with a set of utility R functions to query and analyse its content.
+#'
+#' @docType package
+#' @name synamptome.db
+"_PACKAGE"
