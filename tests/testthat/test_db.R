bf641ead13244d6f6aa373b6d4d0432c791dc4fc
Anatoly Anatoly <lptolik@gmail.com>
2021-08-18 14:01:19 +0100

    test content of the output

--- tests/testthat/test_db.R
+++ tests/testthat/test_db.R
@@ -11,8 +11,22 @@ t <- getGeneInfoByEntrez(1742) #(55 rows)
 expect_equal(dim(t)[1],55)
 t <- getGeneInfoByEntrez('1742')
 #get information for the list of genes
-t <- getGeneInfoByName(c(1741, 1742, 1739, 1740)) #(195 rows)
+t <- getGeneInfoByName(c('DLG4', "Dlg2", 'Dlg3', "Dlg1")) #(195 rows)
 expect_equal(dim(t)[1],195)
+cmp<-list(
+    GeneID=1,
+    Localisation='Postsynaptic',
+    MGI='MGI:1277959',
+    HumanEntrez=1742,
+    MouseEntrez=13385,
+    HumanName='DLG4',
+    MouseName='Dlg4',
+    PaperPMID=10818142,
+    Paper='WALIKONIS_2000',
+    Year=2000,
+    SpeciesTaxID=10116,
+    BrainRegion='Forebrain')
+expect_equivalent(t[1,],cmp)
 #get information for specific gene
 t <- findGenesByEntrez(c(1742, 1741, 1739, 1740)) #(4 rows)
 expect_equal(dim(t)[1],4)
