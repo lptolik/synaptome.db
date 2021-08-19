c339e17c89a8cd7155a38cbc5b7dea8ec6b0ca03
Anatoly Anatoly <lptolik@gmail.com>
2021-08-19 15:21:31 +0100

    PPI in table and graph representation is added.

--- R/ppiTables.R
+++ R/ppiTables.R
@@ -2,17 +2,18 @@
 #' Prepare query for AllMutationAllPapers table
 #'
 #' @return tbl_lazy
-#' @importFrom dplyr tbl select filter pull collect
+#' @importFrom dplyr tbl select filter pull collect distinct
 getPPIQuery<-function(){
     #  gns<-get_dbconn() %>% dplyr::tbl("AllPpiAllPapers") %>%
     gns<-get_dbconn() %>% dplyr::tbl("PPI") %>%
         dplyr::select(
-            ID,#ppiID,
+            # ID,#ppiID,
             A,
-            B,
-            method,
-            type,
-            taxID)
+            B) %>% distinct
+            #,
+            # method,
+            # type,
+            # taxID)
     return(gns)
 }
 
@@ -24,6 +25,9 @@ getPPIQuery<-function(){
 #' @return data.frame
 #' @export
 #' @importFrom dplyr tbl select filter pull collect
+#' @examples
+#' t <- getPPIbyIDs(c(48, 585, 710), type='limited') #(16 rows)
+#' t <- getPPIbyIDs(c(48, 585, 710), type='induced') #306 rows
 getPPIbyIDs <- function(ids, type = c('induced', 'limited')) {
     netType <- match.arg(type)
     gns <- switch (
@@ -61,4 +65,54 @@ getPPIbyName<-function(name, type=c('induced','limited')){
     ids<-getGeneIdByName(name)
     df<-getPPIbyIDs(ids,type)
     return(df)
+}
+
+#' Get table representation of the PPI.
+#'
+#' @param ppi PPI \code{data.frame} with columns A and B, obtaioed
+#' from functions like \code{\link{getPPIbyName}}
+#'
+#' @return table with the following columns added for both interactors (A and B):
+#' \itemize{
+#' \item{GeneID internal database ID}
+#' \item{MGI MGI ID}
+#' \item{HumanEntrez Human Entrez ID}
+#' \item{MouseEntrez Mouse Entrez ID}
+#' \item{HumanName Human gene name}
+#' \item{MouseName Mouse gene name}
+#' \item{RatEntrez Rat Entrez ID}
+#' \item{Rat Name Rat gene name}
+#' }
+
+#' @export
+#' @importFrom dplyr inner_join
+#'
+#' @examples
+#' tbl<-getTableFromPPI(getPPIbyIDs(c(48, 585, 710), type='limited'))
+getTableFromPPI<-function(ppi){
+    aTbl<-getGenesByID(ppi$A)
+    names(aTbl)<-paste0(names(aTbl),'.A')
+    bTbl<-getGenesByID(ppi$B)
+    names(bTbl)<-paste0(names(bTbl),'.B')
+    res<- ppi %>%
+        dplyr::inner_join(aTbl,by=c('A'='GeneID.A')) %>%
+        dplyr::inner_join(bTbl,by=c('B'='GeneID.B'))
+    return(res)
+}
+
+#' Get Igraph representation of PPI
+#'
+#' @param ppi PPI \code{data.frame} with columns A and B, obtaioed
+#' from functions like \code{\link{getPPIbyName}}
+#'
+#' @return
+#' @export
+#' @import igraph
+#'
+#' @examples
+#'
+getIGraphFromPPI<-function(ppi){
+    nids<-unique(c(ppi$A,ppi$B))
+    nodes<-getGenesByID(nids)
+    g<-graph_from_data_frame(ppi,directed = FALSE,vertices = nodes)
 }
--- tests/testthat/test_db.R
+++ tests/testthat/test_db.R
@@ -5,27 +5,36 @@ context("Testing number of returned rows")
 test_that("Proper row number in geneInfo", {
     #get information for specific gene
     t <- getGeneInfoByName('CASK')
-    expect_equal(dim(t)[1], 37)
+    expect_equal(dim(t), c(37,12))
     #get information for the list of genes
     t <- getGeneInfoByName(c('CASK', 'DLG2'))
-    expect_equal(dim(t)[1], 87)
+    expect_equal(dim(t), c(87,12))
     #get information for specific gene
     t <- getGeneInfoByEntrez(1742)
-    expect_equal(dim(t)[1], 55)
+    expect_equal(dim(t), c(55,12))
     t <- getGeneInfoByEntrez('1742')
     #get information for the list of genes
     t <-
         getGeneInfoByName(c('DLG4', "Dlg2", 'Dlg3', "Dlg1"))
-    expect_equal(dim(t)[1], 195)
+    expect_equal(dim(t), c(195,12))
     #get information for specific gene
     t <- findGenesByEntrez(c(1742, 1741, 1739, 1740))
-    expect_equal(dim(t)[1], 4)
+    expect_equal(dim(t), c(4,8))
     #' Find information for gene IDs
     t <- getGeneInfoByIDs (c(48, 585, 710))
-    expect_equal(dim(t)[1], 110)
+    expect_equal(dim(t), c(110,12))
 })
 
 test_that("Proper row number in compartments",{
     c<-getCompartments()
-    expect_equal(dim(c)[1], 3)
+    expect_equal(dim(c), c(3,3))
 })
+
+# test_that("Proper row numbers in PPIs",{
+#     t <- getPPIbyIDs(c(48, 585, 710), type='limited') #(16 rows)
+#     expect_equal(dim(t),c(16,6))
+#     t <- getPPIbyIDs(c(48, 585, 710), type='induced') #306 rows
+#     expect_equal(dim(t),c(306,6))
+#     tbl<-getTableFromPPI(getPPIbyIDs(c(48, 585, 710), type='limited'))
+#     expect_equal(dim(t),c(16,22))
+# })
