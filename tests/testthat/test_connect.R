8ad3f6f3f9ba5e5c3c1ba32c190fa2878ef3897e
Anatoly Anatoly <lptolik@gmail.com>
2021-09-15 20:35:23 +0300

    make environment working

--- tests/testthat/test_connect.R
+++ tests/testthat/test_connect.R
@@ -3,15 +3,19 @@ library(testthat)
 context("Testing connection functions")
 
 test_that("Default connection is working", {
-    expect_true(exists("snptmdb"))
+    expect_true(exists("snptmdbfile",envir = synaptome.db:::snptm_env))
+    snptmdb<-DBI::dbConnect(RSQLite::SQLite(),
+                        get("snptmdbfile",envir = synaptome.db:::snptm_env))
     expect_true(DBI::dbIsValid(snptmdb))
     con <- synaptome.db:::get_dbconn()
-    expect_identical(con, snptmdb)
+    expect_equal(con, snptmdb)
 })
 
 test_that("get_dbconn setup new connection", {
-    DBI::dbDisconnect(snptmdb)
+    synaptome.db:::get_dbconn()
+    DBI::dbDisconnect(get("snptmdb",envir = synaptome.db:::snptm_env))
     con <- synaptome.db:::get_dbconn()
     expect_true(DBI::dbIsValid(con))
-    expect_identical(con, snptmdb)
+    expect_identical(con, get("snptmdb",envir = synaptome.db:::snptm_env))
 })
+
