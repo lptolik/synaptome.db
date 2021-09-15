20574b2d30f04ddb8b28a71de9a0abe4f4e9fa4e
Anatoly Anatoly <lptolik@gmail.com>
2021-09-15 13:09:16 +0300

    proper use of accompany package as a data source

--- R/zzz.R
+++ R/zzz.R
@@ -7,26 +7,58 @@
 #' @import dbplyr
 #' @keywords internal
 get_dbconn <- function() {
-    if (!exists("dbconn") || !DBI::dbIsValid(dbconn)) {
+    if (!exists("mydb") || !DBI::dbIsValid(mydb)) {
         pkgname <- "synaptome.db" # methods::getPackageName()
         dbfile <- system.file("extdata", "synaptome.sqlite", package = pkgname)
-        dbconn <<- DBI::dbConnect(RSQLite::SQLite(), dbfile)
+        mydb <<- DBI::dbConnect(RSQLite::SQLite(), dbfile)
         # cat('DB is connected with ',dbfile)
     }
-    return(dbconn)
+    return(mydb)
 }
 
+#' Hidden load function.
+#' Check the presence of the database and load it with AnnotationHub
+#' if required.
+#' TODO: add timestamp validation.
+#'
+#' @param libname the path where the package is installed
+#' @param pkgname name of the package
+#'
+#' @return dbConnect
+#' @import AnnotationHub
+#' @import synaptome.data
+#' @importFrom utils unzip
+#' @keywords internal
 .onLoad <- function(libname, pkgname) {
     dbfile <- system.file(
         "extdata", "synaptome.sqlite",
         package = pkgname, lib.loc = libname
     )
+    if(!file.exists(dbfile)){
+        ahub <- AnnotationHub::AnnotationHub(localHub=TRUE)
+    sdb<-AnnotationHub::query(ahub,'SynaptomeDB')
+        zipF<-sdb[[1]]
+        l<-unzip(zipF,list=TRUE)
+        fname<-l$Name[which.max(l$Length)]
+        dbpath<-unzip(zipF,files=fname,exdir=tempdir())
+        file.copy(dbpath,paste0(
+            system.file(
+                "extdata",
+                package = pkgname, lib.loc = libname
+            ),.Platform$file.sep,
+            "synaptome.sqlite")
+        )
+        dbfile <- system.file(
+            "extdata", "synaptome.sqlite",
+            package = pkgname, lib.loc = libname
+        )
+    }
     # cat(pkgname,libname)
     db <- dbfile
-    dbconn <<- DBI::dbConnect(RSQLite::SQLite(), dbfile)
+    mydb <<- DBI::dbConnect(RSQLite::SQLite(), dbfile)
 }
 
 
 .onUnload <- function(libpath) {
-    dbDisconnect(get_dbconn())
+    DBI::dbDisconnect(get_dbconn())
 }
